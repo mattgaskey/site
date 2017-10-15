25ac70028ecdcec1fc6c65e72df1aa5ff022772f
mattgaskey <mattgaskey@gmail.com>
2017-10-15 20:46:51 +0300

    Integrated material-ui

---
+++ src/TitleBar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import AppBar from 'material-ui/AppBar';
+
+const TitleBar = () => (
+	<AppBar 
+		title="Matt Gaskey"
+		iconClassNameRight="muidocs-icon-navigation-expand-more"
+	/>
+);
+
+export default TitleBar;
