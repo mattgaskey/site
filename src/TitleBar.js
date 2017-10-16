adb23c4796d9ad570cf63e1263207eab8a3eea47
keepcalling <keepcalling@admins-Air-10.lan>
2017-10-16 14:08:18 +0300

    Added material UI

--- src/TitleBar.js
+++ src/TitleBar.js
@@ -1,11 +1,44 @@
 import React from 'react';
 import AppBar from 'material-ui/AppBar';
 
-const TitleBar = () => (
-	<AppBar 
-		title="Matt Gaskey"
-		iconClassNameRight="muidocs-icon-navigation-expand-more"
-	/>
-);
+class TitleBar extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			scrollY: window.scrollY
+		}
+
+		this.onScroll = this.onScroll.bind(this);
+	}
+
+	componentDidMount() {
+		window.addEventListener('scroll', this.onScroll);
+	}
+
+	onScroll() {
+		this.setState({
+			scrollY: window.scrollY
+		});
+	}
+
+	render() {
+		
+		return (
+			<AppBar 
+				title="Matt Gaskey"
+				iconClassNameRight="muidocs-icon-navigation-expand-more"
+				style={
+					{boxShadow: 'none',
+					position: this.state.scrollY <= 400 ? 'fixed' : 'relative',
+					transition: 'text-align 1s ease-in-out'}
+				}
+				titleStyle={
+					{textAlign: this.state.scrollY >= 64 ? 'center' : 'left'}
+				}
+			/>
+		);
+	}
+
+} 
 
 export default TitleBar;
